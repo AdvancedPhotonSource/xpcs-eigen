c2150b7eaaad1833ad0b778584b8dafc0fb7c7a6
Faisal Khan <faisal.nust@gmail.com>
2020-03-27 11:13:10 -0500

    Added back partials

--- src/xpcs/corr.cpp
+++ src/xpcs/corr.cpp
@@ -632,52 +632,52 @@ void Corr::twotime(data_structure::SparseData *data)
         g2[i * frames + j] /= plist.size();
     }
 
-    // for (int i = 0; i < frames; i++) 
-    // {
-    //   for (int j = i; j < frames; j++) 
-    //   {
-    //     std::vector<int> common_indices;
-    //     std::set_intersection(frame_index[i]->begin(),
-    //                           frame_index[i]->end(),
-    //                           frame_index[j]->begin(),
-    //                           frame_index[j]->end(),
-    //                           back_inserter(common_indices)
-    //                       );
-    //     int idx1 = 0;
-    //     int idx2 = 0;
+    for (int i = 0; i < frames; i++) 
+    {
+      for (int j = i; j < frames; j++) 
+      {
+        std::vector<int> common_indices;
+        std::set_intersection(frame_index[i]->begin(),
+                              frame_index[i]->end(),
+                              frame_index[j]->begin(),
+                              frame_index[j]->end(),
+                              back_inserter(common_indices)
+                          );
+        int idx1 = 0;
+        int idx2 = 0;
 
-    //     for (auto idx : common_indices) {
-    //       auto it1 = std::find(frame_index[i]->begin()+idx1, frame_index[i]->end(), idx);
-    //       auto it2 = std::find(frame_index[j]->begin()+idx2, frame_index[j]->end(), idx);
-    //       idx1 = std::distance(frame_index[i]->begin(), it1);
-    //       idx2 = std::distance(frame_index[j]->begin(), it2);
+        for (auto idx : common_indices) {
+          auto it1 = std::find(frame_index[i]->begin()+idx1, frame_index[i]->end(), idx);
+          auto it2 = std::find(frame_index[j]->begin()+idx2, frame_index[j]->end(), idx);
+          idx1 = std::distance(frame_index[i]->begin(), it1);
+          idx2 = std::distance(frame_index[j]->begin(), it2);
 
 
-    //       g2[i * frames + j] += (frame_value[i]->at(idx1) * frame_value[j]->at(idx2));
-    //     }
+          g2[i * frames + j] += (frame_value[i]->at(idx1) * frame_value[j]->at(idx2));
+        }
 
-    //     g2[i * frames + j] /= plist.size();
-    //   }
-    // }
+        g2[i * frames + j] /= plist.size();
+      }
+    }
 
     g2_pointers[binIdx] = g2;
 
-    // for (int ff = 0; ff < frames; ff++) {
-    //   int count = 0;
-    //   int windowno = 0;
-    //   for (int fx = 0, fy = ff; fx < frames - ff; fx++, fy++) {
-    //     g2full[ff] += g2[fx*frames + fy];
+    for (int ff = 0; ff < frames; ff++) {
+      int count = 0;
+      int windowno = 0;
+      for (int fx = 0, fy = ff; fx < frames - ff; fx++, fy++) {
+        g2full[ff] += g2[fx*frames + fy];
 
 
-    //     if (windowno < total_partials && ff < wsize) {
-    //       g2partial[ff * total_partials + windowno] += g2[fx*frames + fy];
-    //     }
+        if (windowno < total_partials && ff < wsize) {
+          g2partial[ff * total_partials + windowno] += g2[fx*frames + fy];
+        }
         
-    //     windowno = (fx+1) / wsize;
-    //     count++;
-    //   }
-    //   g2full[ff] /= count;
-    // }
+        windowno = (fx+1) / wsize;
+        count++;
+      }
+      g2full[ff] /= count;
+    }
     
     g2full_pointers[binIdx] = g2full;
 
